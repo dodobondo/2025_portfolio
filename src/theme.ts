14ccdc33b1f7407060e2cc272c65b8075eeb48d8
dodobondo <kishito11@yahoo.co.jp>
2025-06-27 04:14:02 +0900

    テーマフォントの決定

--- src/theme.ts
+++ src/theme.ts
@@ -7,15 +7,8 @@ import { body } from "framer-motion/client";
 
 // カスタムフォント
 const fonts = {
-  // heading: "segoe-ui, hiragino-kaku-gothic-pron, sans-serif;",
   heading: "oscine, zen-kaku-gothic-new, sans-serif",
-  // heading: "m-plus-rounded-2c, sans-serif",
-  
-  // body: "segoe-ui, hiragino-kaku-gothic-pron, sans-serif;",
-  // body: "oscine, fot-cezanne-pron, sans-serif",
-  // body: "m-plus-rounded-2c, sans-serif",
   body: "zen-kaku-gothic-new, sans-serif",
-  // body: "segoe-ui, dnp-shuei-mgothic-std, sans-serif",
 };
 // カスタムテーマの作成（chacra uiの上書き）
 const customTheme = extendTheme({ fonts});
